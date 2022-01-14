9f13c2664454df057688d9f891cbaebfe41a3088
NoePerezFab <noeperez@e-gisnet.com>
2022-01-14 16:59:24 -0600

    Llamados funcionando

--- src/main/java/com/gisnet/egisfil/controller/LlamadosController.java
+++ src/main/java/com/gisnet/egisfil/controller/LlamadosController.java
@@ -9,12 +9,14 @@ import com.gisnet.egisfil.repositoryservice.TicketRepositoryService;
 import com.gisnet.egisfil.repositoryservice.TurnoAtendidoRepositoryService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class LlamadosController {
     @Autowired
